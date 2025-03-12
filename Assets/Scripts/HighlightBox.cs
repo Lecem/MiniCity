180408f3508e67bcd3bf0a698a9eb529a915ac27
Lecem <lecem.gokal@gmail.com>
2025-03-12 14:19:40 +0300

    v0.3 Building Name Box Set and NameBox Rotating Added

--- Assets/Scripts/HighlightBox.cs
+++ Assets/Scripts/HighlightBox.cs
@@ -5,12 +5,18 @@ public class HighlightBox : MonoBehaviour
 {
     private MeshRenderer lastObject; // En son etkileþime girilen obje
     public string buildingName; //binanýn adýný tutacak
-    public GameObject nameUI; //UI objesini tutacak.
+    public GameObject nameUI; //world canvas UI objesini tutacak.
+
+    private TextMeshProUGUI nameText;
+
+    public float yHigh = 2f;
 
     void Start()
     {
          lastObject = GetComponent<MeshRenderer>();
         lastObject.enabled = false;   
+
+        
 
     }
 
@@ -18,14 +24,26 @@ public class HighlightBox : MonoBehaviour
     {
         Debug.Log(gameObject + "objeye gelindi");
 
-        if (nameUI == null)
+        if (nameUI == null )
         {
             Debug.LogError(gameObject.name + "için nameUI atanmadý ki! ");
             return; //hata varsa yine de fonksiyondan çýk
         }
+
         nameUI.SetActive(true); //UI ý görünür yapar.
+       
         nameUI.GetComponent<TextMeshProUGUI>().text = buildingName; //UI ögesine binanýn adýný koyar.
-         lastObject.enabled = true;
+
+
+        //UI ý bina üstüne taþý
+        nameUI.transform.position = transform.position + Vector3.up * yHigh;
+
+        //UI kameraya baktýrýr
+        nameUI.transform.LookAt(Camera.main.transform);
+        nameUI.transform.Rotate(0, 180, 0);
+
+
+        lastObject.enabled = true;
 
     }
 
